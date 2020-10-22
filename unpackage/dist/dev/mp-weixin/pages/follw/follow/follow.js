d6aa07a62eb573e45ec6a932ad2e559d3cc86d41
hefang <hefang@qq.com>
2020-10-22 15:15:08 +0800

    tabbar配置

---
+++ unpackage/dist/dev/mp-weixin/pages/follw/follow/follow.js
@@ -0,0 +1,20 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/follw/follow/follow"],{
+
+/***/ 19:
+/*!***********************************************************************************!*\
+  !*** C:/Users/何方/Desktop/news/main.js?{"page":"pages%2Ffollw%2Ffollow%2Ffollow"} ***!
+  \***********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _follow = _interopRequireDefault(__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module './pages/follw/follow/follow.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_follow.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ })
+
+},[[19,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../../.sourcemap/mp-weixin/pages/follw/follow/follow.js.map
